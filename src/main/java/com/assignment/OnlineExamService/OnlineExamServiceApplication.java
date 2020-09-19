6a79a7b649e47c3d1f84733af4b9a60d55f783e0
shubham <shubham.sharma_1@cleartrip.com>
2020-09-19 15:50:32 +0530

    Initial setup done

---
+++ src/main/java/com/assignment/OnlineExamService/OnlineExamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.OnlineExamService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamServiceApplication.class, args);
+	}
+
+}
